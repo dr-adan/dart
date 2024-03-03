4ba8bcbc47b87e1db027a65e2c3244efd4ab5d0a
dr-adan <dr.adan.sheikh@gmail.com>
2024-03-03 12:07:47 -0800

    first commit

---
+++ variables.dart
@@ -0,0 +1,15 @@
+void main() {
+  // declaring variables
+  String name = "Abdijabar";
+  String address = "Nairobi";
+  num age = 20;
+  num height = 6.4;
+  bool isMarried = true;
+
+  //printing variables value
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Marital Status is $isMarried");
+}
